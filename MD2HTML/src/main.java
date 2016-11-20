15148ce57018af4cd6d3e4f2378006d7e0899493
이승환 <21400549@handong.edu>
2016-11-20 23:52:31 +0900

    class들 만드는 중

---
+++ MD2HTML/src/main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class main 
+{
+	public static void main(String[] args) throws IOException
+	{
+		// cli part >> (CLI class)
+		// data, type 받
+		
+		// Document Object 만듦 
+		
+		// MDParser Object 만듦 
+		
+		// Parsing() >> Document Object 에 node_list 저장
+		
+		// Document Object.accept(plainVisitor) 
+		
+		// Document Object.htmlData >> file output 
+		
+		//end.
+		
+	}
+}
+
+
