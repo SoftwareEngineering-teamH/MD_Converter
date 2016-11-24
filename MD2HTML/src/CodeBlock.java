e6ce593a14d15772f165bdb77777db7b3bd6f828
이승환 <21400549@handong.edu>
2016-11-25 00:27:37 +0900

    간단한 md 구분까지는 가능!

--- MD2HTML/src/CodeBlock.java
+++ MD2HTML/src/CodeBlock.java
@@ -1,5 +1,11 @@
 
 public class CodeBlock extends Node implements MDElement
 {
-
+	public Node create(String data)
+	{
+		this.md_data = data;
+		this.node_type = 2;
+		
+		return this;
+	}
 }
--- MD2HTML/src/ItemList.java
+++ MD2HTML/src/ItemList.java
@@ -1,5 +1,11 @@
 
 public class ItemList extends Node implements MDElement
 {
-
+	public Node create(String data)
+	{
+		this.md_data = data;
+		this.node_type = 4;
+		
+		return this;
+	}
 }
