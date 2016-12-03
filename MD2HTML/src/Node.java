7a722f18f2f5ba22a6fcae11f9e4e69bbb19f794
scsky6332 <scsky6332@naver.com>
2016-12-04 02:13:44 +0900

    set tokens

--- MD2HTML/src/Node.java
+++ MD2HTML/src/Node.java
@@ -34,116 +34,118 @@ public class Node implements MDElement
 	{	
 		int length = md_data.length();
 		int[] token_arr = new int[length];
-		int[] stack_style = new int[length];
-		//init
-		for(int i=0; i<token_arr.length;i++)
-			token_arr[i]=0;
-		//init stack_style
-		for(int i=0; i<stack_style.length;i++)
-			stack_style[i]=stack_style.length;
-		int j = 0;
-		int em_count = 0;
-		int strong_count=0;
-		int three_over = 1;
-		boolean style_strong_flag = false;
-		boolean style_em_flag = true;
-		//boolean buf_zero_style = false;
-		// style - algorithm
-		for(int i = 0; i<token_arr.length; i++)
+		
+		// initialize
+		for(int i=0;i<token_arr.length;i++)
 		{
-			//style
-			if(md_data.charAt(i) == '*' && token_arr[i] == 0 )
+				token_arr[i] = 0;
+		}
+		
+		
+		for(int i=0;i<token_arr.length;i++)
+		{
+			int flag = 0;
+
+			try
 			{
-				//if(i ==0)buf_zero_style = true;
-				
-				if(i+1<token_arr.length && md_data.charAt(i) == '*' && md_data.charAt(i+1) == '*'){
-					style_strong_flag = true;
-					style_em_flag=false;
-					three_over++;
-					if(three_over>=3){
-						token_arr[i-1] = token_arr[i] = token_arr[i+1] =0;
-					}
-					
-				}else if(i-1>=0&& md_data.charAt(i) == '*' && md_data.charAt(i-1) != '*'){
-					style_strong_flag = false;
-					style_em_flag=true;
-					three_over = 0;
-				}
-				if(style_strong_flag == true && style_em_flag==false)strong_count++;
-				if(style_strong_flag == false && style_em_flag==true)em_count++;
-					//count=j+1;
-				//this stack is to know which index have *
-				stack_style[j] = i;
-				
-				if(em_count == 1 && style_em_flag == true && style_strong_flag ==false){
-				token_arr[i] = 2;
-				//count = 0;
-				}
-				
-				else if(em_count == 2 && style_em_flag == true && style_strong_flag ==false){
-					token_arr[i] = 22;
-					em_count=0;
-					stack_style[j] = stack_style[j-1]=stack_style.length;;
-					j= j-2;
-				}
-				
-				//strong
-				else if(strong_count == 1 &&style_strong_flag == true && style_em_flag ==false){
-					token_arr[i] = 2;
-					
-				}
-				
-				else if(strong_count == 2&&style_strong_flag == true&& style_em_flag ==false){
-					token_arr[i] = 2;
-					//count = 0;
-				}
-				else if(strong_count == 3&&style_strong_flag == true&& style_em_flag ==false){
-					token_arr[i] = 22;
-					//count = 0;
-				}
-				else if(strong_count == 4&&style_strong_flag == true&& style_em_flag ==false){
-					token_arr[i] = 22;
-					strong_count = 0;
-					//count = 0;
-					style_strong_flag = false;
-					style_em_flag = true;
-					stack_style[j] = stack_style[j-1] = stack_style[j-2] =stack_style[j-3] =stack_style.length;;
-					j= j-4;
-				}
-				
-				
-				
-					j++;
-				//style_strong_flag = false;
-				/*try
+				// strong tag
+				if(md_data.charAt(i)=='*' && md_data.charAt(i+1)=='*' && token_arr[i] == 0 && i+1 < token_arr.length)
 				{
-					if(md_data.charAt(i+1) == '*')
-						token_arr[i+1] = 2;
-				}
-				catch(Exception e)
-				{
-					// String index error
-				}
-					
-				for(int j = i+2; j<token_arr.length;j++)
-				{//jump more one block to detect 
-					if(md_data.charAt(j) == '*' && token_arr[j] == 0)
-					{
-						token_arr[j] = 22;
-						if(md_data.charAt(j+1) == '*')
+					flag = 1;
+					for(int j=i+2;j<token_arr.length;j++){
+						if(md_data.charAt(j)=='*' && md_data.charAt(j+1)=='*' && token_arr[j] == 0 && flag == 1){
+							token_arr[i] = 2;
+							token_arr[i+1] = 2;		
+							token_arr[j] = 22;
 							token_arr[j+1] = 22;
+							break;
+						}
+					}
+				}
+			} catch(Exception e)
+			{ 
+				// String index error
+			}
+			
+			// em tag
+			if(md_data.charAt(i) == '*' && token_arr[i] == 0 && i < token_arr.length)
+			{
+				flag = 1;
+				for(int j=i+1; j < token_arr.length; j++)
+				{
+					if(md_data.charAt(j)=='*' && token_arr[j] == 0 && flag == 1 && j < token_arr.length)
+					{
+						token_arr[i] = 2;
+						token_arr[j] = 22;
 						break;
 					}
-				}*/
+				}
+			}	
+			
+			// link token
+			if(md_data.charAt(i) == '[' && token_arr[i] == 0 && i < token_arr.length)
+			{
+				for(int j=i+1; j<token_arr.length; j++)
+				{
+					if(md_data.charAt(j) == ']' && token_arr[j] == 0 && j < token_arr.length)
+					{
+						for(int k = j+1; k < token_arr.length; k++)
+						{
+							if(md_data.charAt(k) == '(' && token_arr[k] == 0 && k < token_arr.length)
+							{
+								for(int l = k+1; l < token_arr.length; l++)
+								{
+									if(md_data.charAt(l) == ')' && token_arr[l] == 0 && l < token_arr.length)
+									{
+										token_arr[i] = 4;
+										token_arr[j] = 44;
+										token_arr[k] = 444;
+										token_arr[l] = 4444;
+									}
+								}
+							}
+						}
+					}
+				}
 			}
+				
+			// image token
+			if(md_data.charAt(i) == '!' && token_arr[i] == 0 && i < token_arr.length)
+			{
+				for(int j=i+1; j<token_arr.length; j++)
+				{
+					if(md_data.charAt(j) == '[' && token_arr[j] == 0 && j < token_arr.length)
+					{
+						for(int k = j+1; k < token_arr.length; k++)
+						{
+							if(md_data.charAt(k) == ']' && token_arr[k] == 0 && k < token_arr.length)
+							{
+								for(int l = k+1; l < token_arr.length; l++)
+								{
+									if(md_data.charAt(l) == '(' && token_arr[l] == 0 && l < token_arr.length)
+									{
+										for(int m = l+1; m < token_arr.length; m++)
+										{
+											if(md_data.charAt(m) == ')' && token_arr[m] == 0 && l < token_arr.length)
+											{
+												token_arr[i] = 5;
+												token_arr[j] = 55;
+												token_arr[k] = 555;
+												token_arr[l] = 5555;
+												token_arr[m] = 55555;
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+							
 		}
-		//´Ù µ¹°í¼­ *Â¦ÀÌ ¸ÂÁö ¾Ê´Â °ÍµéÀº ¸ðµÎ 0À¸·Î ¹Ù²ãÁÖ±â. 
-		for(int k=0;k<stack_style.length;k++){
-			if(stack_style[k] != stack_style.length)token_arr[stack_style[k]] = 0;
-			else break;
-		}
-		//if(buf_zero_style == true)token_arr[0] = 0;
-		
+
 		
 		for(int i = 0; i<md_data.length(); i++)
 		{
@@ -191,7 +193,7 @@ public class Node implements MDElement
 			return "<h6>";
 		case 2:
 			return "<pre><code>";
-		// ItemList´Â ¾ÆÁ÷ Á¤È®È÷ Ã³¸®ÇÏÁö ¾ÊÀº »óÅÂ (ordered/unordered ±¸º°¸¸ ÇØ ³õÀ½)
+		// ItemListï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½È®ï¿½ï¿½ Ã³ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ (ordered/unordered ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½)
 		case 4:
 			return "<ul><li>";
 		case 44:
